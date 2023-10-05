959a2210f0eaafbe092f8627ccc0543e3b7ce2d0
Tony Glass <idttip.tg@gmail.com>
2023-10-05 13:53:59 -0700

    completedlab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(message= 'roller-skate') {
+    return `This Saturday, I want to ${message}!`
+} 
+
+function mondayWork(message= 'go to the office') {
+    return `This Monday, I will ${message}.`
+}
+
+function wrapAdjective(flair= "*") {
+    return function (message= "special") {
+        return `You are ${flair}${message}${flair}!`
+
+    }
+}
